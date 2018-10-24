5a66b92275ab5f0aaeeb3b7bb830dc6a278ea20b
Chenmi <cmlingyi@qq.com>
2018-10-24 18:38:28 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,11 @@ module.exports = {
     sourceType: 'module'
   },
   env: {
-    browser: true
+    browser: true,
+    es6: true
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
+  extends: ['standard', 'prettier'],
   // required to lint *.wpy files
   plugins: [
     'html'
