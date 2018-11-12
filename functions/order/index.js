ee4cf5d7de0186c63160376ec88b3c4d3ed58ed6
Chenmi <cmlingyi@qq.com>
2018-11-12 11:20:37 +0800

    update

--- functions/order/index.js
+++ functions/order/index.js
@@ -5,19 +5,21 @@ cloud.init()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  let { OPENID } = cloud.getWXContext();
   const db = cloud.database();
   const classCollection = db.collection('class');
+  const _ = db.command;
+  const classId = event.classId;
+  const newList = event.newList;
   try {
-    return await classCollection.add({
-      data: {
-        title: event.title,
-        list: event.list,
-        openId: OPENID,
-        createdAt: new Date()
-      }
-    })
+    return await classCollection
+      .doc(classId)
+      .update({
+        data: {
+          list: _.set(newList)
+        }
+      })
   } catch (err) {
     console.error(err);
   }
+
 }
